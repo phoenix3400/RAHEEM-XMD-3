f0de0c813b0e3e3b908c96a11a7f525fe9fb619b
Raheem-cm <nyoniraheem@gmail.com>
2025-06-20 23:11:48 +0300

    Update tag-admins.js

--- plugins/tag-admins.js
+++ plugins/tag-admins.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${admin.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ HAIKO ┃ MDX ✪──";
+        teks += "└──✪ RAHEEM ┃ XMD-3 ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: admins }, { quoted: mek });
 
