898dd19a5a17ea9dd09056b2134fa92b1c1651b9
Raheem-cm <nyoniraheem@gmail.com>
2025-06-21 00:29:46 +0300

    Update define.js

--- plugins/define.js
+++ plugins/define.js
@@ -31,7 +31,7 @@ async (conn, mek, m, { from, q, reply }) => {
 ✍️ *Example*: ${example}  
 📝 *Synonyms*: ${synonyms}  
 
-> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜᴀɪᴋᴏ ᴍᴅx*`;
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ RAHEEM-CM*`;
 
         if (audio) {
             await conn.sendMessage(from, { audio: { url: audio }, mimetype: 'audio/mpeg' }, { quoted: mek });
