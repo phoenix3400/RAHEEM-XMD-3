abc2d0fe6e3c88093f00208e18fa1604ad87f171
Raheem-cm <nyoniraheem@gmail.com>
2025-06-21 00:30:46 +0300

    Update dl-fb.js

--- plugins/dl-fb.js
+++ plugins/dl-fb.js
@@ -31,7 +31,7 @@ cmd({
     const videoUrl = data.data.url;
     await conn.sendMessage(from, {
       video: { url: videoUrl },
-      caption: "📥 *Facebook Video Downloaded*\n\n- > *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜᴀɪᴋᴏ ᴍᴅx* ✅",
+      caption: "📥 *Facebook Video Downloaded*\n\n- > *ᴘᴏᴡᴇʀᴇᴅ ʙʏ RAHEEM-CM* ✅",
     }, { quoted: m });
 
   } catch (error) {
