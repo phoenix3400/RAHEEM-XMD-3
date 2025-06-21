755f73c0aa2abbbee046f9ec1919b1fa538c9bbd
Raheem-cm <nyoniraheem@gmail.com>
2025-06-21 20:37:15 +0300

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  > *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜᴀɪᴋᴏ ᴍᴅx*
+  > *ᴘᴏᴡᴇʀᴇᴅ ʙʏ RAHEEM-CM*
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
