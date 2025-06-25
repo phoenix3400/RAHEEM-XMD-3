ca05d625bdf1ed399b4197bafba6772a849b8ee8
Raheem-cm <nyoniraheem@gmail.com>
2025-06-25 03:48:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KAISEN~",
+SESSION_ID: process.env.SESSION_ID || "KAISEN~3lBjlQwD#-A1_JI5SRs_9-YeELd0Kot38hVwt8lD1yGD2lH502pA",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -25,7 +25,7 @@ MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/xp1wdz.jpg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || "+",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "RAHEEM-XMD-3",
 // add bot namw here for menu
