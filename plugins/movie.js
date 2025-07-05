e987045b1627e0af836e04e17fa82fd38b173148
Raheem-cm <nyoniraheem@gmail.com>
2025-07-05 16:47:26 +0300

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -62,7 +62,7 @@ async (conn, mek, m, { from, reply, sender, args }) => {
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363372853772240@newsletter',
+                        newsletterJid: '120363399470975987@newsletter',
                         newsletterName: 'RAHEEM-XMD-3',
                         serverMessageId: 143
                     }
