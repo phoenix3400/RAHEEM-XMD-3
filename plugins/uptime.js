46e46b7795f40ad88363e3d4b8531c2e7c58de15
Raheem-cm <nyoniraheem@gmail.com>
2025-07-05 13:11:04 +0300

    Update uptime.js

--- plugins/uptime.js
+++ plugins/uptime.js
@@ -103,7 +103,7 @@ async (conn, mek, m, { from, reply, args }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363398101781980@newsletter',
+                    newsletterJid: '120363399470975987@newsletter',
                     newsletterName: 'RAHEEM-CM',
                     serverMessageId: 143
                 }
