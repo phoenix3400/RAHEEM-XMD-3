733ce18d2060161e9f7de3ed14c96131ae0c7a59
Raheem-cm <nyoniraheem@gmail.com>
2025-07-05 18:39:25 +0300

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -55,7 +55,7 @@ cmd({
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363372853772240@newsletter',
+          newsletterJid: '120363399470975987@newsletter',
           newsletterName: 'RAHEEM-XMD-3',
           serverMessageId: 143
         }
