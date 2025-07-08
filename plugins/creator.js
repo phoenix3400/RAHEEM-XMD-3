da1287c2ad74c2509883f664f18551bde1a3513d
Raheem-cm <nyoniraheem@gmail.com>
2025-07-08 10:26:28 +0300

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,31 +1,16 @@
-cmd({
-  pattern: 'cr',
+module.exports = {
+  name: 'cr',
   alias: ['creator', 'owner'],
-  react: '👤',
-  desc: 'Show the creator contact info.',
   category: 'info',
-  use: '.cr',
-  filename: __filename
-}, async (bot, msg, msgInfo, { from, reply }) => {
-  try {
+  desc: 'Show the bot creator contact info.',
+  react: '👤',
+  usage: '.cr',
+  async exec(msg, sock, args, from, command, quoted, m) {
+    const creatorName = 'Raheem-cm';
     const creatorNumber = '255763111390';
-    const vcard = `BEGIN:VCARD
-VERSION:3.0
-FN:Creator
-TEL;type=CELL;type=VOICE;waid=${creatorNumber}:${creatorNumber}
-END:VCARD`;
 
-    await bot.sendMessage(from, {
-      contacts: {
-        displayName: 'Bot Creator',
-        contacts: [{ vcard }]
-      }
-    }, { quoted: msg });
+    const text = `👤 *Bot Creator Info*\n\n👑 *Name:* ${creatorName}\n📞 *Contact:* wa.me/${creatorNumber}\n\n_© ${creatorName}_`;
 
-    await reply('👤 *Bot Creator Contact Shared Successfully!*');
-
-  } catch (err) {
-    console.log(err);
-    await reply('❌ Error sending creator contact.');
+    await sock.sendMessage(from, { text }, { quoted: msg });
   }
-});
+};
