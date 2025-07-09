6dfeb3e31980849f328a43ee0656ffa3b2ea4f4a
Raheem-cm <nyoniraheem@gmail.com>
2025-07-09 16:56:36 +0300

    Update auto-voice.js

--- plugins/auto-voice.js
+++ plugins/auto-voice.js
@@ -4,7 +4,7 @@ const { cmd } = require("../command");
 
 cmd({ on: "body" }, async (conn, m, msg, { from, body }) => {
   try {
-    const jsonUrl = "https://raw.githubusercontent.com/Raheem-cm/RAHEEM-XMD-3-assets/main/autovoice.json";
+    const jsonUrl = "https://raw.githubusercontent.com/Raheem-cm/RAHEEM-XMD-3-data/main/autovoice.json";
     const res = await axios.get(jsonUrl);
     const voiceMap = res.data;
 
