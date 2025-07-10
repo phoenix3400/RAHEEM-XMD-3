c730e37ccb8dc8c6ee5e715a806a1f033fac0d77
Raheem-cm <nyoniraheem@gmail.com>
2025-07-10 14:11:21 +0300

    Update auto-reply.js

--- plugins/auto-reply.js
+++ plugins/auto-reply.js
@@ -6,7 +6,7 @@ const {cmd , commands} = require('../command')
 
 
 // Replace this with your actual GitHub RAW JSON URL
-const GITHUB_RAW_URL = 'https://raw.githubusercontent.com/criss-vevo/CRISS-DATA/main/autoreply.json';
+const GITHUB_RAW_URL = 'https://raw.githubusercontent.com/Raheem-cm/RAHEEM-XMD-3-DATA/main/autoreply.json';
 
 cmd({
   on: "body"
