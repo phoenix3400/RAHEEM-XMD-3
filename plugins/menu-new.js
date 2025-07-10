7b4de15345bd93321214eaa569734bdd08a7133d
Raheem-cm <nyoniraheem@gmail.com>
2025-07-10 21:50:57 +0300

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -296,7 +296,7 @@ _✨ Powered by RAHEEM-CM🎗${config.OWNER_NAME} ✨_
                 return await conn.sendMessage(
                     from,
                     {
-                        gitf: { url: config.MENU_VIDEO_URL || 'https://files.catbox.moe/skv4x7.mp4' },
+                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/aq881d.jpg' },
                         caption: menuCaption,
                         contextInfo
                     },
