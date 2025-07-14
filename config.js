c4c1f1f3986c3ed295be4ab68f4dffce7c32d339
Raheem-cm <nyoniraheem@gmail.com>
2025-07-14 09:18:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "RAHEEM-XMD-2>>>v0RXARiT#mqJQp_k3ScOqe9sf0QBMGqgQOUCkDQDh8U-ShUNgY38",
+SESSION_ID: process.env.SESSION_ID || "RAHEEM-XMD-2>>>",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
