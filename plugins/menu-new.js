2ecbed74f336eb9a9b8322b94ca55419775ff367
Raheem-cm <nyoniraheem@gmail.com>
2025-07-17 17:13:08 +0300

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -9,145 +9,1046 @@ cmd({
     filename: __filename
 }, async (conn, mek, m, { from }) => {
     try {
-        // Styles Array
+        // 4 UNIQUE RANDOM STYLES (vertical commands)
         const styles = [
-`╭──❖  ${config.BOT_NAME || '𝑹𝑨𝑯𝑬𝑬𝑴-𝑿𝑴𝑫-3'}  ❖──╮
+
+`╭─────❖  ${config.BOT_NAME || 'RAHEEM-XMD-3'}  ❖─────╮
 ┃ 👑 Owner: ${config.OWNER_NAME}
 ┃ 🛠️ Mode: ${config.MODE}
 ┃ 🕒 Version: 2.0.0
 ┃ 🧭 Time: ${config.Timenow}
 ┃ 🔖 Prefix: ${config.PREFIX}
-╰────────────────────────────╯ powerd by *sir Raheem-cm*
+╰────────────────────────────╯
 
 ╭─❖ *🏠 MAIN COMMANDS* ❖─╮
-┃ ping┃ ping2┃ speed┃ live┃ alive
-┃ runtime┃ uptime┃ repo┃ owner┃ menu┃ menu2┃ restart
-╰────────────────────────────╯ powerd by *sir Raheem-cm*
+┃ ping
+┃ ping2
+┃ speed
+┃ live
+┃ alive
+┃ runtime
+┃ uptime
+┃ repo
+┃ owner
+┃ menu
+┃ menu2
+┃ restart
+╰────────────────────────────╯
 
 ╭─❖ *📥 DOWNLOAD MENU* ❖─╮
-┃ facebook┃ mediafire┃ tiktok┃ twitter┃ insta┃ apk┃ img┃ tt2┃ pins
-┃ apk2┃ fb2┃ pinterest┃ spotify┃ play┃ play2┃ audio┃ video┃ video2
-┃ ytmp3┃ ytmp4┃ song┃ darama┃ gdrive┃ ssweb┃ tiks
-╰────────────────────────────╯ powerd by *sir Raheem-cm*
+┃ facebook
+┃ mediafire
+┃ tiktok
+┃ twitter
+┃ insta
+┃ apk
+┃ img
+┃ tt2
+┃ pins
+┃ apk2
+┃ fb2
+┃ pinterest
+┃ spotify
+┃ play
+┃ play2
+┃ audio
+┃ video
+┃ video2
+┃ ytmp3
+┃ ytmp4
+┃ song
+┃ darama
+┃ gdrive
+┃ ssweb
+┃ tiks
+╰────────────────────────────╯
 
 ╭─❖ *👥 GROUP MENU* ❖─╮
-┃ grouplink┃ kickall┃ kickall2┃ kickall3┃ add┃ remove┃ kick┃ promote┃ demote┃ dismiss
-┃ revoke┃ setgoodbye┃ setwelcome┃ delete┃ getpic┃ ginfo┃ disappear on┃ disappear off
-┃ disappear 7D,24H┃ allreq┃ updategname┃ updategdesc┃ joinrequests┃ senddm┃ nikal┃ mute┃ unmute
-┃ lockgc┃ unlockgc┃ invite┃ tag┃ hidetag┃ tagall┃ tagadmins
-╰────────────────────────────╯ powerd by *sir Raheem-cm*
+┃ grouplink
+┃ kickall
+┃ kickall2
+┃ kickall3
+┃ add
+┃ remove
+┃ kick
+┃ promote
+┃ demote
+┃ dismiss
+┃ revoke
+┃ setgoodbye
+┃ setwelcome
+┃ delete
+┃ getpic
+┃ ginfo
+┃ disappear on
+┃ disappear off
+┃ disappear 7D,24H
+┃ allreq
+┃ updategname
+┃ updategdesc
+┃ joinrequests
+┃ senddm
+┃ nikal
+┃ mute
+┃ unmute
+┃ lockgc
+┃ unlockgc
+┃ invite
+┃ tag
+┃ hidetag
+┃ tagall
+┃ tagadmins
+╰────────────────────────────╯
 
 ╭─❖ *🎉 FUN MENU* ❖─╮
-┃ shapar┃ rate┃ insult┃ hack┃ ship┃ character┃ pickup┃ joke┃ hrt┃ hpy┃ syd┃ anger┃ shy
-┃ kiss┃ mon┃ cunfuzed┃ setpp┃ hand┃ nikal┃ hold┃ hug┃ hifi┃ poke
-╰────────────────────────────╯ powerd by *sir Raheem-cm*
+┃ shapar
+┃ rate
+┃ insult
+┃ hack
+┃ ship
+┃ character
+┃ pickup
+┃ joke
+┃ hrt
+┃ hpy
+┃ syd
+┃ anger
+┃ shy
+┃ kiss
+┃ mon
+┃ cunfuzed
+┃ setpp
+┃ hand
+┃ nikal
+┃ hold
+┃ hug
+┃ hifi
+┃ poke
+╰────────────────────────────╯
 
 ╭─❖ *👑 OWNER MENU* ❖─╮
-┃ owner┃ menu┃ menu2┃ vv┃ listcmd┃ allmenu┃ repo┃ block┃ unblock┃ fullpp┃ setpp┃ restart┃ shutdown
-┃ updatecmd┃ alive┃ ping┃ gjid┃ jid
-╰────────────────────────────╯ powerd by *sir Raheem-cm*
+┃ owner
+┃ menu
+┃ menu2
+┃ vv
+┃ listcmd
+┃ allmenu
+┃ repo
+┃ block
+┃ unblock
+┃ fullpp
+┃ setpp
+┃ restart
+┃ shutdown
+┃ updatecmd
+┃ alive
+┃ ping
+┃ gjid
+┃ jid
+╰────────────────────────────╯
 
 ╭─❖ *🤖 AI MENU* ❖─╮
-┃ ai┃ gpt3┃ gpt2┃ gptmini┃ gpt┃ meta┃ blackbox┃ luma┃ dj┃ khan┃ jawad
-┃ gpt4┃ bing┃ imagine┃ imagine2┃ copilot
-╰────────────────────────────╯ powerd by *sir Raheem-cm*
+┃ ai
+┃ gpt3
+┃ gpt2
+┃ gptmini
+┃ gpt
+┃ meta
+┃ blackbox
+┃ luma
+┃ dj
+┃ khan
+┃ jawad
+┃ gpt4
+┃ bing
+┃ imagine
+┃ imagine2
+┃ copilot
+╰────────────────────────────╯
 
 ╭─❖ *🖌️ ANIME MENU* ❖─╮
-┃ fack┃ truth┃ dare┃ dog┃ awoo┃ garl┃ waifu┃ neko┃ megnumin
-┃ maid┃ loli┃ animegirl┃ animegirl1┃ animegirl2┃ animegirl3┃ animegirl4
-┃ animegirl5┃ anime1┃ anime2┃ anime3┃ anime4┃ anime5┃ animenews┃ foxgirl┃ naruto
-╰────────────────────────────╯ powerd by *sir Raheem-cm*
+┃ fack
+┃ truth
+┃ dare
+┃ dog
+┃ awoo
+┃ garl
+┃ waifu
+┃ neko
+┃ megnumin
+┃ maid
+┃ loli
+┃ animegirl
+┃ animegirl1
+┃ animegirl2
+┃ animegirl3
+┃ animegirl4
+┃ animegirl5
+┃ anime1
+┃ anime2
+┃ anime3
+┃ anime4
+┃ anime5
+┃ animenews
+┃ foxgirl
+┃ naruto
+╰────────────────────────────╯
 
 ╭─❖ *🔄 CONVERT MENU* ❖─╮
-┃ sticker┃ sticker2┃ emojimix┃ fancy┃ take┃ tomp3┃ tts┃ trt┃ base64┃ unbase64
-┃ binary┃ dbinary┃ tinyurl┃ urldecode┃ urlencode┃ url┃ repeat┃ ask┃ readmore
-╰────────────────────────────╯ powerd by *sir Raheem-cm*
+┃ sticker
+┃ sticker2
+┃ emojimix
+┃ fancy
+┃ take
+┃ tomp3
+┃ tts
+┃ trt
+┃ base64
+┃ unbase64
+┃ binary
+┃ dbinary
+┃ tinyurl
+┃ urldecode
+┃ urlencode
+┃ url
+┃ repeat
+┃ ask
+┃ readmore
+╰────────────────────────────╯
 
 ╭─❖ *📌 OTHER MENU* ❖─╮
-┃ timenow┃ date┃ count┃ calculate┃ countx┃ flip┃ coinflip┃ rcolor┃ roll┃ fact
-┃ cpp┃ rw┃ pair┃ pair2┃ pair3┃ fancy┃ logo <text>┃ define┃ news┃ movie┃ weather
-┃ srepo┃ insult┃ save┃ wikipedia┃ gpass┃ githubstalk┃ yts┃ ytv
-╰────────────────────────────╯ powerd by *sir Raheem-cm*
+┃ timenow
+┃ date
+┃ count
+┃ calculate
+┃ countx
+┃ flip
+┃ coinflip
+┃ rcolor
+┃ roll
+┃ fact
+┃ cpp
+┃ rw
+┃ pair
+┃ pair2
+┃ pair3
+┃ fancy
+┃ logo <text>
+┃ define
+┃ news
+┃ movie
+┃ weather
+┃ srepo
+┃ insult
+┃ save
+┃ wikipedia
+┃ gpass
+┃ githubstalk
+┃ yts
+┃ ytv
+╰────────────────────────────╯
 
 ╭─❖ *💞 REACTIONS MENU* ❖─╮
-┃ bully┃ cuddle┃ cry┃ hug┃ awoo┃ kiss┃ lick┃ pat┃ smug┃ bonk┃ yeet┃ blush
-┃ smile┃ wave┃ highfive┃ handhold┃ nom┃ bite┃ glomp┃ slap┃ kill┃ happy
-┃ wink┃ poke┃ dance┃ cringe
-╰────────────────────────────╯ powerd by *sir Raheem-cm*
+┃ bully
+┃ cuddle
+┃ cry
+┃ hug
+┃ awoo
+┃ kiss
+┃ lick
+┃ pat
+┃ smug
+┃ bonk
+┃ yeet
+┃ blush
+┃ smile
+┃ wave
+┃ highfive
+┃ handhold
+┃ nom
+┃ bite
+┃ glomp
+┃ slap
+┃ kill
+┃ happy
+┃ wink
+┃ poke
+┃ dance
+┃ cringe
+╰────────────────────────────╯
 
 _✨ Powered by RAHEEM-CM🎗${config.OWNER_NAME} ✨_`,
 
-`┏━━◆ ${config.BOT_NAME || '𝑹𝑨𝑯𝑬𝑬𝑴-𝑿𝑴𝑫-3'} MAIN MENU ◆━━┓
-┃ 👑 Owner: ${config.OWNER_NAME}   🛠️ Mode: ${config.MODE}
-┃ 🕒 Version: 2.0.0   🧭 Time: ${config.Timenow}
-┃ 🔖 Prefix: ${config.PREFIX}
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+`▄▀▄▀▄ ${config.BOT_NAME || 'RAHEEM-XMD-3'} ▄▀▄▀▄
 
-🏠 *MAIN:* ping, ping2, speed, live, alive, runtime, uptime, repo, owner, menu, menu2, restart
+👑 Owner: ${config.OWNER_NAME}
+🛠️ Mode: ${config.MODE}
+🕒 Version: 2.0.0
+🧭 Time: ${config.Timenow}
+🔖 Prefix: ${config.PREFIX}
 
-📥 *DOWNLOAD:* facebook, mediafire, tiktok, twitter, insta, apk, img, tt2, pins, apk2, fb2, pinterest, spotify, play, play2, audio, video, video2, ytmp3, ytmp4, song, darama, gdrive, ssweb, tiks
+🎯 MAIN COMMANDS:
+- ping
+- ping2
+- speed
+- live
+- alive
+- runtime
+- uptime
+- repo
+- owner
+- menu
+- menu2
+- restart
 
-👥 *GROUP:* grouplink, kickall, kickall2, kickall3, add, remove, kick, promote, demote, dismiss, revoke, setgoodbye, setwelcome, delete, getpic, ginfo, disappear on, disappear off, disappear 7D,24H, allreq, updategname, updategdesc, joinrequests, senddm, nikal, mute, unmute, lockgc, unlockgc, invite, tag, hidetag, tagall, tagadmins
+⬇️ DOWNLOAD MENU:
+- facebook
+- mediafire
+- tiktok
+- twitter
+- insta
+- apk
+- img
+- tt2
+- pins
+- apk2
+- fb2
+- pinterest
+- spotify
+- play
+- play2
+- audio
+- video
+- video2
+- ytmp3
+- ytmp4
+- song
+- darama
+- gdrive
+- ssweb
+- tiks
 
-🎉 *FUN:* shapar, rate, insult, hack, ship, character, pickup, joke, hrt, hpy, syd, anger, shy, kiss, mon, cunfuzed, setpp, hand, nikal, hold, hug, hifi, poke
+👥 GROUP MENU:
+- grouplink
+- kickall
+- kickall2
+- kickall3
+- add
+- remove
+- kick
+- promote
+- demote
+- dismiss
+- revoke
+- setgoodbye
+- setwelcome
+- delete
+- getpic
+- ginfo
+- disappear on
+- disappear off
+- disappear 7D,24H
+- allreq
+- updategname
+- updategdesc
+- joinrequests
+- senddm
+- nikal
+- mute
+- unmute
+- lockgc
+- unlockgc
+- invite
+- tag
+- hidetag
+- tagall
+- tagadmins
 
-👑 *OWNER:* owner, menu, menu2, vv, listcmd, allmenu, repo, block, unblock, fullpp, setpp, restart, shutdown, updatecmd, alive, ping, gjid, jid
+🎉 FUN MENU:
+- shapar
+- rate
+- insult
+- hack
+- ship
+- character
+- pickup
+- joke
+- hrt
+- hpy
+- syd
+- anger
+- shy
+- kiss
+- mon
+- cunfuzed
+- setpp
+- hand
+- nikal
+- hold
+- hug
+- hifi
+- poke
 
-🤖 *AI:* ai, gpt3, gpt2, gptmini, gpt, meta, blackbox, luma, dj, khan, jawad, gpt4, bing, imagine, imagine2, copilot
+👑 OWNER MENU:
+- owner
+- menu
+- menu2
+- vv
+- listcmd
+- allmenu
+- repo
+- block
+- unblock
+- fullpp
+- setpp
+- restart
+- shutdown
+- updatecmd
+- alive
+- ping
+- gjid
+- jid
 
-🖌️ *ANIME:* fack, truth, dare, dog, awoo, garl, waifu, neko, megnumin, maid, loli, animegirl, animegirl1, animegirl2, animegirl3, animegirl4, animegirl5, anime1, anime2, anime3, anime4, anime5, animenews, foxgirl, naruto
+🤖 AI MENU:
+- ai
+- gpt3
+- gpt2
+- gptmini
+- gpt
+- meta
+- blackbox
+- luma
+- dj
+- khan
+- jawad
+- gpt4
+- bing
+- imagine
+- imagine2
+- copilot
 
-🔄 *CONVERT:* sticker, sticker2, emojimix, fancy, take, tomp3, tts, trt, base64, unbase64, binary, dbinary, tinyurl, urldecode, urlencode, url, repeat, ask, readmore
+🖌️ ANIME MENU:
+- fack
+- truth
+- dare
+- dog
+- awoo
+- garl
+- waifu
+- neko
+- megnumin
+- maid
+- loli
+- animegirl
+- animegirl1
+- animegirl2
+- animegirl3
+- animegirl4
+- animegirl5
+- anime1
+- anime2
+- anime3
+- anime4
+- anime5
+- animenews
+- foxgirl
+- naruto
 
-📌 *OTHER:* timenow, date, count, calculate, countx, flip, coinflip, rcolor, roll, fact, cpp, rw, pair, pair2, pair3, fancy, logo <text>, define, news, movie, weather, srepo, insult, save, wikipedia, gpass, githubstalk, yts, ytv
+🔄 CONVERT MENU:
+- sticker
+- sticker2
+- emojimix
+- fancy
+- take
+- tomp3
+- tts
+- trt
+- base64
+- unbase64
+- binary
+- dbinary
+- tinyurl
+- urldecode
+- urlencode
+- url
+- repeat
+- ask
+- readmore
 
-💞 *REACTIONS:* bully, cuddle, cry, hug, awoo, kiss, lick, pat, smug, bonk, yeet, blush, smile, wave, highfive, handhold, nom, bite, glomp, slap, kill, happy, wink, poke, dance, cringe
+📌 OTHER MENU:
+- timenow
+- date
+- count
+- calculate
+- countx
+- flip
+- coinflip
+- rcolor
+- roll
+- fact
+- cpp
+- rw
+- pair
+- pair2
+- pair3
+- fancy
+- logo <text>
+- define
+- news
+- movie
+- weather
+- srepo
+- insult
+- save
+- wikipedia
+- gpass
+- githubstalk
+- yts
+- ytv
 
-━━━[ Powered by RAHEEM-CM🎗${config.OWNER_NAME} ]━━━`,
+💞 REACTIONS MENU:
+- bully
+- cuddle
+- cry
+- hug
+- awoo
+- kiss
+- lick
+- pat
+- smug
+- bonk
+- yeet
+- blush
+- smile
+- wave
+- highfive
+- handhold
+- nom
+- bite
+- glomp
+- slap
+- kill
+- happy
+- wink
+- poke
+- dance
+- cringe
 
-`╔═══════════════╗
-║   ${config.BOT_NAME || '𝑹𝑨𝑯𝑬𝑬𝑴-𝑿𝑴𝑫-3'} MENU   ║
-╠═══════════════╣
+━━━ Powered by RAHEEM-CM🎗${config.OWNER_NAME} ━━━`,
+
+`┏━━━━━ 『 ${config.BOT_NAME || 'RAHEEM-XMD-3'} 』━━━━━┓
+┃ 👑  Owner: ${config.OWNER_NAME}
+┃ 🛠️  Mode : ${config.MODE}
+┃ 🕒  Version: 2.0.0
+┃ 🧭  Time: ${config.Timenow}
+┃ 🔖  Prefix: ${config.PREFIX}
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+🏠 MAIN COMMANDS
+┃ ping
+┃ ping2
+┃ speed
+┃ live
+┃ alive
+┃ runtime
+┃ uptime
+┃ repo
+┃ owner
+┃ menu
+┃ menu2
+┃ restart
+
+📥 DOWNLOAD MENU
+┃ facebook
+┃ mediafire
+┃ tiktok
+┃ twitter
+┃ insta
+┃ apk
+┃ img
+┃ tt2
+┃ pins
+┃ apk2
+┃ fb2
+┃ pinterest
+┃ spotify
+┃ play
+┃ play2
+┃ audio
+┃ video
+┃ video2
+┃ ytmp3
+┃ ytmp4
+┃ song
+┃ darama
+┃ gdrive
+┃ ssweb
+┃ tiks
+
+👥 GROUP MENU
+┃ grouplink
+┃ kickall
+┃ kickall2
+┃ kickall3
+┃ add
+┃ remove
+┃ kick
+┃ promote
+┃ demote
+┃ dismiss
+┃ revoke
+┃ setgoodbye
+┃ setwelcome
+┃ delete
+┃ getpic
+┃ ginfo
+┃ disappear on
+┃ disappear off
+┃ disappear 7D,24H
+┃ allreq
+┃ updategname
+┃ updategdesc
+┃ joinrequests
+┃ senddm
+┃ nikal
+┃ mute
+┃ unmute
+┃ lockgc
+┃ unlockgc
+┃ invite
+┃ tag
+┃ hidetag
+┃ tagall
+┃ tagadmins
+
+🎉 FUN MENU
+┃ shapar
+┃ rate
+┃ insult
+┃ hack
+┃ ship
+┃ character
+┃ pickup
+┃ joke
+┃ hrt
+┃ hpy
+┃ syd
+┃ anger
+┃ shy
+┃ kiss
+┃ mon
+┃ cunfuzed
+┃ setpp
+┃ hand
+┃ nikal
+┃ hold
+┃ hug
+┃ hifi
+┃ poke
+
+👑 OWNER MENU
+┃ owner
+┃ menu
+┃ menu2
+┃ vv
+┃ listcmd
+┃ allmenu
+┃ repo
+┃ block
+┃ unblock
+┃ fullpp
+┃ setpp
+┃ restart
+┃ shutdown
+┃ updatecmd
+┃ alive
+┃ ping
+┃ gjid
+┃ jid
+
+🤖 AI MENU
+┃ ai
+┃ gpt3
+┃ gpt2
+┃ gptmini
+┃ gpt
+┃ meta
+┃ blackbox
+┃ luma
+┃ dj
+┃ khan
+┃ jawad
+┃ gpt4
+┃ bing
+┃ imagine
+┃ imagine2
+┃ copilot
+
+🖌️ ANIME MENU
+┃ fack
+┃ truth
+┃ dare
+┃ dog
+┃ awoo
+┃ garl
+┃ waifu
+┃ neko
+┃ megnumin
+┃ maid
+┃ loli
+┃ animegirl
+┃ animegirl1
+┃ animegirl2
+┃ animegirl3
+┃ animegirl4
+┃ animegirl5
+┃ anime1
+┃ anime2
+┃ anime3
+┃ anime4
+┃ anime5
+┃ animenews
+┃ foxgirl
+┃ naruto
+
+🔄 CONVERT MENU
+┃ sticker
+┃ sticker2
+┃ emojimix
+┃ fancy
+┃ take
+┃ tomp3
+┃ tts
+┃ trt
+┃ base64
+┃ unbase64
+┃ binary
+┃ dbinary
+┃ tinyurl
+┃ urldecode
+┃ urlencode
+┃ url
+┃ repeat
+┃ ask
+┃ readmore
+
+📌 OTHER MENU
+┃ timenow
+┃ date
+┃ count
+┃ calculate
+┃ countx
+┃ flip
+┃ coinflip
+┃ rcolor
+┃ roll
+┃ fact
+┃ cpp
+┃ rw
+┃ pair
+┃ pair2
+┃ pair3
+┃ fancy
+┃ logo <text>
+┃ define
+┃ news
+┃ movie
+┃ weather
+┃ srepo
+┃ insult
+┃ save
+┃ wikipedia
+┃ gpass
+┃ githubstalk
+┃ yts
+┃ ytv
+
+💞 REACTIONS MENU
+┃ bully
+┃ cuddle
+┃ cry
+┃ hug
+┃ awoo
+┃ kiss
+┃ lick
+┃ pat
+┃ smug
+┃ bonk
+┃ yeet
+┃ blush
+┃ smile
+┃ wave
+┃ highfive
+┃ handhold
+┃ nom
+┃ bite
+┃ glomp
+┃ slap
+┃ kill
+┃ happy
+┃ wink
+┃ poke
+┃ dance
+┃ cringe
+
+━━━ Powered by RAHEEM-CM🎗${config.OWNER_NAME} ━━━`,
+
+`╔═════ ${config.BOT_NAME || 'RAHEEM-XMD-3'} ═════╗
 ║ 👑 Owner: ${config.OWNER_NAME}
 ║ 🛠️ Mode: ${config.MODE}
 ║ 🕒 Version: 2.0.0
 ║ 🧭 Time: ${config.Timenow}
 ║ 🔖 Prefix: ${config.PREFIX}
-╚═══════════════╝
+╠═══════════════════════════╣
 
-🏠 *MAIN*: ping, ping2, speed, live, alive, runtime, uptime, repo, owner, menu, menu2, restart
-📥 *DOWNLOAD*: facebook, mediafire, tiktok, twitter, insta, apk, img, tt2, pins, apk2, fb2, pinterest, spotify, play, play2, audio, video, video2, ytmp3, ytmp4, song, darama, gdrive, ssweb, tiks
-👥 *GROUP*: grouplink, kickall, kickall2, kickall3, add, remove, kick, promote, demote, dismiss, revoke, setgoodbye, setwelcome, delete, getpic, ginfo, disappear on, disappear off, disappear 7D,24H, allreq, updategname, updategdesc, joinrequests, senddm, nikal, mute, unmute, lockgc, unlockgc, invite, tag, hidetag, tagall, tagadmins
-🎉 *FUN*: shapar, rate, insult, hack, ship, character, pickup, joke, hrt, hpy, syd, anger, shy, kiss, mon, cunfuzed, setpp, hand, nikal, hold, hug, hifi, poke
-👑 *OWNER*: owner, menu, menu2, vv, listcmd, allmenu, repo, block, unblock, fullpp, setpp, restart, shutdown, updatecmd, alive, ping, gjid, jid
-🤖 *AI*: ai, gpt3, gpt2, gptmini, gpt, meta, blackbox, luma, dj, khan, jawad, gpt4, bing, imagine, imagine2, copilot
-🖌️ *ANIME*: fack, truth, dare, dog, awoo, garl, waifu, neko, megnumin, maid, loli, animegirl, animegirl1, animegirl2, animegirl3, animegirl4, animegirl5, anime1, anime2, anime3, anime4, anime5, animenews, foxgirl, naruto
-🔄 *CONVERT*: sticker, sticker2, emojimix, fancy, take, tomp3, tts, trt, base64, unbase64, binary, dbinary, tinyurl, urldecode, urlencode, url, repeat, ask, readmore
-📌 *OTHER*: timenow, date, count, calculate, countx, flip, coinflip, rcolor, roll, fact, cpp, rw, pair, pair2, pair3, fancy, logo <text>, define, news, movie, weather, srepo, insult, save, wikipedia, gpass, githubstalk, yts, ytv
-💞 *REACTIONS*: bully, cuddle, cry, hug, awoo, kiss, lick, pat, smug, bonk, yeet, blush, smile, wave, highfive, handhold, nom, bite, glomp, slap, kill, happy, wink, poke, dance, cringe
+🏠 MAIN COMMANDS
+║ ping
+║ ping2
+║ speed
+║ live
+║ alive
+║ runtime
+║ uptime
+║ repo
+║ owner
+║ menu
+║ menu2
+║ restart
 
-_✨ Powered by RAHEEM-CM🎗${config.OWNER_NAME} ✨_`,
+📥 DOWNLOAD MENU
+║ facebook
+║ mediafire
+║ tiktok
+║ twitter
+║ insta
+║ apk
+║ img
+║ tt2
+║ pins
+║ apk2
+║ fb2
+║ pinterest
+║ spotify
+║ play
+║ play2
+║ audio
+║ video
+║ video2
+║ ytmp3
+║ ytmp4
+║ song
+║ darama
+║ gdrive
+║ ssweb
+║ tiks
 
-`✦ ${config.BOT_NAME || '𝑹𝑨𝑯𝑬𝑬𝑴-𝑿𝑴𝑫-3'} Main Menu ✦
+👥 GROUP MENU
+║ grouplink
+║ kickall
+║ kickall2
+║ kickall3
+║ add
+║ remove
+║ kick
+║ promote
+║ demote
+║ dismiss
+║ revoke
+║ setgoodbye
+║ setwelcome
+║ delete
+║ getpic
+║ ginfo
+║ disappear on
+║ disappear off
+║ disappear 7D,24H
+║ allreq
+║ updategname
+║ updategdesc
+║ joinrequests
+║ senddm
+║ nikal
+║ mute
+║ unmute
+║ lockgc
+║ unlockgc
+║ invite
+║ tag
+║ hidetag
+║ tagall
+║ tagadmins
 
-👑 *Owner*: ${config.OWNER_NAME}
-🛠️ *Mode*: ${config.MODE}
-🕒 *Version*: 2.0.0
-🧭 *Time*: ${config.Timenow}
-🔖 *Prefix*: ${config.PREFIX}
+🎉 FUN MENU
+║ shapar
+║ rate
+║ insult
+║ hack
+║ ship
+║ character
+║ pickup
+║ joke
+║ hrt
+║ hpy
+║ syd
+║ anger
+║ shy
+║ kiss
+║ mon
+║ cunfuzed
+║ setpp
+║ hand
+║ nikal
+║ hold
+║ hug
+║ hifi
+║ poke
 
-🏠 *MAIN*: ping, ping2, speed, live, alive, runtime, uptime, repo, owner, menu, menu2, restart
-📥 *DOWNLOAD*: facebook, mediafire, tiktok, twitter, insta, apk, img, tt2, pins, apk2, fb2, pinterest, spotify, play, play2, audio, video, video2, ytmp3, ytmp4, song, darama, gdrive, ssweb, tiks
-👥 *GROUP*: grouplink, kickall, kickall2, kickall3, add, remove, kick, promote, demote, dismiss, revoke, setgoodbye, setwelcome, delete, getpic, ginfo, disappear on, disappear off, disappear 7D,24H, allreq, updategname, updategdesc, joinrequests, senddm, nikal, mute, unmute, lockgc, unlockgc, invite, tag, hidetag, tagall, tagadmins
-🎉 *FUN*: shapar, rate, insult, hack, ship, character, pickup, joke, hrt, hpy, syd, anger, shy, kiss, mon, cunfuzed, setpp, hand, nikal, hold, hug, hifi, poke
-👑 *OWNER*: owner, menu, menu2, vv, listcmd, allmenu, repo, block, unblock, fullpp, setpp, restart, shutdown, updatecmd, alive, ping, gjid, jid
-🤖 *AI*: ai, gpt3, gpt2, gptmini, gpt, meta, blackbox, luma, dj, khan, jawad, gpt4, bing, imagine, imagine2, copilot
-🖌️ *ANIME*: fack, truth, dare, dog, awoo, garl, waifu, neko, megnumin, maid, loli, animegirl, animegirl1, animegirl2, animegirl3, animegirl4, animegirl5, anime1, anime2, anime3, anime4, anime5, animenews, foxgirl, naruto
-🔄 *CONVERT*: sticker, sticker2, emojimix, fancy, take, tomp3, tts, trt, base64, unbase64, binary, dbinary, tinyurl, urldecode, urlencode, url, repeat, ask, readmore
-📌 *OTHER*: timenow, date, count, calculate, countx, flip, coinflip, rcolor, roll, fact, cpp, rw, pair, pair2, pair3, fancy, logo <text>, define, news, movie, weather, srepo, insult, save, wikipedia, gpass, githubstalk, yts, ytv
-💞 *REACTIONS*: bully, cuddle, cry, hug, awoo, kiss, lick, pat, smug, bonk, yeet, blush, smile, wave, highfive, handhold, nom, bite, glomp, slap, kill, happy, wink, poke, dance, cringe
+👑 OWNER MENU
+║ owner
+║ menu
+║ menu2
+║ vv
+║ listcmd
+║ allmenu
+║ repo
+║ block
+║ unblock
+║ fullpp
+║ setpp
+║ restart
+║ shutdown
+║ updatecmd
+║ alive
+║ ping
+║ gjid
+║ jid
 
-━━━ Powered by RAHEEM-CM🎗${config.OWNER_NAME} ━━━`
+🤖 AI MENU
+║ ai
+║ gpt3
+║ gpt2
+║ gptmini
+║ gpt
+║ meta
+║ blackbox
+║ luma
+║ dj
+║ khan
+║ jawad
+║ gpt4
+║ bing
+║ imagine
+║ imagine2
+║ copilot
+
+🖌️ ANIME MENU
+║ fack
+║ truth
+║ dare
+║ dog
+║ awoo
+║ garl
+║ waifu
+║ neko
+║ megnumin
+║ maid
+║ loli
+║ animegirl
+║ animegirl1
+║ animegirl2
+║ animegirl3
+║ animegirl4
+║ animegirl5
+║ anime1
+║ anime2
+║ anime3
+║ anime4
+║ anime5
+║ animenews
+║ foxgirl
+║ naruto
+
+🔄 CONVERT MENU
+║ sticker
+║ sticker2
+║ emojimix
+║ fancy
+║ take
+║ tomp3
+║ tts
+║ trt
+║ base64
+║ unbase64
+║ binary
+║ dbinary
+║ tinyurl
+║ urldecode
+║ urlencode
+║ url
+║ repeat
+║ ask
+║ readmore
+
+📌 OTHER MENU
+║ timenow
+║ date
+║ count
+║ calculate
+║ countx
+║ flip
+║ coinflip
+║ rcolor
+║ roll
+║ fact
+║ cpp
+║ rw
+║ pair
+║ pair2
+║ pair3
+║ fancy
+║ logo <text>
+║ define
+║ news
+║ movie
+║ weather
+║ srepo
+║ insult
+║ save
+║ wikipedia
+║ gpass
+║ githubstalk
+║ yts
+║ ytv
+
+💞 REACTIONS MENU
+║ bully
+║ cuddle
+║ cry
+║ hug
+║ awoo
+║ kiss
+║ lick
+║ pat
+║ smug
+║ bonk
+║ yeet
+║ blush
+║ smile
+║ wave
+║ highfive
+║ handhold
+║ nom
+║ bite
+║ glomp
+║ slap
+║ kill
+║ happy
+║ wink
+║ poke
+║ dance
+║ cringe
+
+╚════════ Powered by RAHEEM-CM🎗${config.OWNER_NAME} ═══════╝`
         ];
 
         // Random style
